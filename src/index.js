5cc6c18dcd5b6056d6cc80595ee70e9863fa457e
Nanda Vikas <nandavikas858@gmail.com>
2020-10-11 23:25:43 +0530

    make it better

--- src/index.js
+++ src/index.js
@@ -28,8 +28,8 @@ io.on('connection', (socket) => {
 
         socket.join(user.room)
 
-        socket.emit('message', generateMessage('Admin','Welcome!'))
-        socket.broadcast.to(user.room).emit('message', generateMessage('Admin',`${user.username} has joined the chat`))
+        socket.emit('message', generateMessage('Chat Admin','Welcome!'))
+        socket.broadcast.to(user.room).emit('message', generateMessage('Chat Admin',`${user.username} has joined the chat`))
 
         io.to(user.room).emit('roomData',{
             room: user.room,
@@ -60,7 +60,7 @@ io.on('connection', (socket) => {
     socket.on('disconnect', () => {
         const user = removeUser(socket.id)
         if (user) {
-            io.to(user.room).emit('message', generateMessage('Admin',`${user.username} has left`))
+            io.to(user.room).emit('message', generateMessage('Chat Admin',`${user.username} has left`))
             io.to(user.room).emit('roomData',{
                 room: user.room,
                 users: getUserInRoom(user.room)
